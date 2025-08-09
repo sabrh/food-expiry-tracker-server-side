cc513f38ef6101edb73bd241fc60b6f63c69452d
Sabrina Haque <sabrina.haque442@gmail.com>
2025-08-09 18:48:22 +0600

    vercel

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const port=process.env.PORT || 3000
 app.use(cors())
 app.use(express.json())
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lcrd7ak.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
